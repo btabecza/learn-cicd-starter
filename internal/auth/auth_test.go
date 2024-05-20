a2bb5caaf1a25ff8ed84c1540607edc3bd2611ad
BT <btabeche23@gmail.com>
2024-05-20 20:18:40 +0200

    Add unit test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,28 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	headers := http.Header{}
+	headers.Add("Authorization", "ApiKey 123456")
+
+	apiKey, err := GetAPIKey(headers)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if apiKey != "12345" {
+		t.Errorf("Expected '123456', got '%s'", apiKey)
+	}
+
+	headers = http.Header{}
+	headers.Add("Authorization", "Bearer 123456")
+
+	_, err = GetAPIKey(headers)
+	if err == nil {
+		t.Errorf("Expected error for malformed authorization header, got nil")
+	}
+}
